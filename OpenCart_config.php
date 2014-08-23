b9c479b2495eddfe8932d6c5947c62c775a712c1
Pak Ricard <pakricard@gmail.com>
2014-08-23 09:28:44 +0100

    Initial commit of opencart bridge

---
+++ OpenCart_config.php
@@ -0,0 +1,17 @@
+<?php
+
+/*Configuration file for webERP-OpenCart Connector 
+Edit all this file to provide the information for the connector to communicate with webERP and OpenCart */
+
+$Version = '0.01';
+
+$Language ='en_GB';
+
+$opencart_db_user = 'username';
+$opencart_db_pwd = 'password';
+$opencart_db_host = 'localhost';
+$opencart_db_name = 'opencart_database_name';
+$opencart_db_tableprefix ='oc_';
+
+
+?>
