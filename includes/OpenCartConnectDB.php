9ee64d903da4433f54a0f4477355d3aedea20930
Tim Schofield <tim@weberpafrica.com>
2014-10-11 14:35:59 +0100

    New function for running DB queries against the open cart database

--- includes/OpenCartConnectDB.php
+++ includes/OpenCartConnectDB.php
@@ -25,4 +25,42 @@ if ( !$db_oc ) {
 	exit;
 }
 
+function DB_query_oc($SQL, $ErrorMessage = '', $DebugMessage = '', $Transaction = false, $TrapErrors = true) {
+
+	global $db_oc;
+	global $PathPrefix;
+
+	$Result = mysqli_query($db_oc, $SQL);
+
+	if ($DebugMessage == '') {
+		$DebugMessage = _('The SQL that failed was');
+	}
+
+	if (DB_error_no($db_oc) != 0 and $TrapErrors == true) {
+		if ($TrapErrors) {
+			require_once($PathPrefix . 'includes/header.inc');
+		}
+		prnMsg($ErrorMessage . '<br />' . DB_error_msg(), 'error', _('Database Error') . ' ' . DB_error_no());
+		if ($Debug == 1) {
+			prnMsg($DebugMessage . '<br />' . $SQL . '<br />', 'error', _('Database SQL Failure'));
+		}
+		if ($Transaction) {
+			$SQL = 'rollback';
+			$Result = DB_query_oc($SQL);
+			if (DB_error_no() != 0) {
+				prnMsg(_('Error Rolling Back Transaction'), 'error', _('Database Rollback Error') . ' ' . DB_error_no());
+			} else {
+				prnMsg(_('Rolling Back Transaction OK'), 'error', _('Database Rollback Due to Error Above'));
+			}
+		}
+		if ($TrapErrors) {
+			include($PathPrefix . 'includes/footer.inc');
+			exit;
+		}
+	}
+
+	return $Result;
+
+}
+
 ?>
